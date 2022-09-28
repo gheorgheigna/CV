83ff7d5064771b3b7150616a5ccf29875fbddcd5
gheorgheigna <113337736+gheorgheigna@users.noreply.github.com>
2022-09-28 23:48:50 +0300

    Add files via upload

--- sidebar_app.js
+++ sidebar_app.js
@@ -1,11 +1,60 @@
+
+
+
+document.getElementById("openclose").addEventListener("click", contorclick); 
+let contor=0;
+function contorclick(){
+   contor++;
+
+}
+function openclose() {
+  
+  if ((document.getElementById("sidebar").style.width==" " || "0px") && (contor%2)==0) {
+    openNav();
+    
+    
+    
+  } 
+  if ((document.getElementById("sidebar").style.width  == "210px") && (contor%2)==1) {
+    closeNav();
+    
+    
+  
+  }
+    
+  
+}
 function openNav() {
-    document.getElementById("sidebar").style.width = "150px";
-    document.getElementById("content").style.marginLeft = "150px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+  document.getElementById("sidebar").style.width = "210px";
+  document.getElementById("content").style.marginLeft = "210px";
+    
   }
   
-  function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("content").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("content").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
   }
+
+
+
+
+
+
+
+
+
+
+function openPage(pageName,elmnt) {
+  let i, tabcontent;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  console.log(tabcontent.length);
+  console.log(pageName);
+  document.getElementById(pageName).style.display = "block";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
